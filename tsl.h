5d76ce0ae7ed41802795616e46fc9bc070df2233
korpe <korpe@cs.bilkent.edu.tr>
2024-03-01 10:20:35 +0300

    Update tsl.h

--- tsl.h
+++ tsl.h
@@ -1,8 +1,8 @@
 #ifndef _TSL_H_
 #define _TSL_H_
 
-// do not change this header file (tsh.h) 
-// it is the interface of the library to the applications
+// do not change this header file (tsl.h) 
+// it is the interface of the tsl library to the applications
 
 #include <ucontext.h>
 
